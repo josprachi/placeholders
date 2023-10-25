7c711427d55fae14ca9674e09a92fe35c5adfd9e
Prachi Joshi <josprachi@yahoo.com>
2023-10-25 10:37:25 +0530

    Add files via upload

---
+++ QuizDataNew.js
@@ -0,0 +1,238 @@
+export default questions = 
+ [
+  { "questiontype": "text_",
+    "question": "What would you like to call you? ",
+
+    "options": [
+      " Enter Your Name",
+      " Enter Your Name",
+      " Enter Your Name",
+      " Enter Your Name",
+    ],
+
+  },
+  {
+    "questiontype": "GenderSelection",
+    "question": "I identify As",
+    "options": [
+      "Male",
+      "Female",
+      "Other",
+      
+    ],
+  
+  },
+  
+  {
+    "questiontype": "Option Selection",
+    "question": "My Age is*",
+    "options": [
+      " < 18 year old",
+      "18 -24",
+      "25 -34",
+      "35-44",
+      "45 - 59",
+      "60 - 74",
+      "> 75 year old"
+      
+    ],
+  
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": "In what form do you consume nicotine? *",
+    "options": [
+      "Smoking cigarettes",
+      "Vaping",
+      "CHewing tobacco (chewing, dipping, snusing, etc)",
+      
+    ],
+  
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": "I started Smoking --- years ago   , No of cigarettes I currently smoke , I have already Quit *",
+    "options": [
+     "",
+     " ",
+     "",
+     "",
+    ],
+   
+  },
+  {
+    "questiontype": "text_",
+    "question": "Text...",
+    "options": [
+     "",
+     " ",
+     "",
+     "",
+    ],
+   
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": "I pay  --- rs per cigarette    *",
+    "options": [
+     "",
+     " ",
+     "",
+     "",
+    ],
+  
+  },
+  {
+    "questiontype": "text_",
+    "question": "Did you know",
+    "options": [
+     "",
+     " ",
+     "",
+     "",
+    ],
+   
+  },
+  {
+    "questiontype": "text_",
+    "question": "Did you know",
+    "options": [
+     "",
+     " ",
+     "",
+     "",
+    ],
+   
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": "Which of the smoking related health issues are you facing currently ?*",
+    "options": [
+      "Tiredness",
+      "shortness of breath",
+      "Asthama/COPD",
+      "Regular cough/ cold /flu",
+      "cancer",
+      "High blood presuure",
+      "mood issues",
+      "sexual health/ impotance/fertility",
+      "other",
+      " No health issues",
+      
+    ],
+   
+  },
+  {
+    "questiontype": "text_",
+    "question": "Guess what user name?*",
+    "options": [
+      "",
+     " ",
+     "",
+     "",
+      
+    ],
+  
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": " Where do you stand in quit smoking Journey?*",
+    "options": [
+      "I have decided to quit smoking",
+      "I have decided to reduce my smoking",
+      "I am not sure whether I want to quit smokng or not",
+      "I will quit later",
+      "I never want to quit smoking",
+      
+      
+    ],
+  
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": " What does quitting smoking mean to you?*",
+    "options": [
+      "Better health",
+      "Mental Peace",
+      "Making a loved one happy",
+      "Saving money",
+      "Having more energy for daily life",
+      "More time for hobbies",   
+      
+    ],
+  
+  },
+
+  {
+    "questiontype": "Option Selection",
+    "question": " How many times have tried to quit in the past?*",
+    "options": [
+      "Never",
+      "1-2 Times",
+      "3-10 Times",
+      "Lost Count",
+      
+      
+    ],
+   
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": " Which technique have you used to try and quit? *",
+    "options": [
+      "Cold Turkey",
+      "Cutting down slowly",
+      "Nicotine gums/patches",
+      "Medication (e.g. champix,Wellbutrin, etc)",
+      "Vaping",
+      "In person psycological program/ Consultations",
+      "Book or video program(easyway,whyQuit, etc)",
+      "A mobile App(Non-QuitSure)",
+      "Other",   
+          ],
+  
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": "  What challenges did you face in your previous quitting attempts that caused you to relapse?*",
+    "options": [
+      "Couldn't resist cravings",
+      "Withdrawal symptoms",
+      "Weight gain",
+      "Anxiety/restlessness",
+      "Lack of focus",
+      "Wanted just one cigarette",      
+      "Other",   
+          ],
+  
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": "  What challenges did you face in your previous quitting attempts that caused you to relapse?*",
+    "options": [
+      "Couldn't resist cravings",
+      "Withdrawal symptoms",
+      "Weight gain",
+      "Anxiety/restlessness",
+      "Lack of focus",
+      "Wanted just one cigarette",      
+      "Other",   
+          ],
+   
+  },
+  {
+    "questiontype": "Option Selection",
+    "question": "  I Believe taht smoking/vaping/chewing..*",
+    "options": [
+      "Tastes good",
+      "Relieves my stress",
+      "Relaxes me",
+      "Helps during boredom",
+      "Increases focus on work",
+      "Gives me a high",
+      "Helps in social situations",      
+      
+          ],
+   
+  },
+];
